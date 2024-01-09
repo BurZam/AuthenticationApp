285df7828fe12f80ecd6fb3506bc58e94d641b7c
BurZam <143757393+BurZam@users.noreply.github.com>
2024-01-09 13:40:20 -0600

    Add project files.

---
+++ AuthenticationApp/Client/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+	return <h1>Authentication App</h1>;
+}
+
+export default App;
---
+++ AuthenticationApp/Controllers/AuthenticationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthenticationController: ControllerBase
+    {
+        private readonly ILogger<AuthenticationController> _logger;
+
+        public AuthenticationController(ILogger<AuthenticationController> logger)
+        {
+            this._logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
